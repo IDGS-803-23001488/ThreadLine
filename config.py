39d881601caa2b78690ee10a7a739adc9c6bf079
IDGS-803-23001488 <agtz50538@gmail.com>
2026-03-20 19:34:50 -0600

    20/03/2026. AngelG. ft_redesing. cambios en config, ahora se requiere config_local.py para funcionar

--- config.py
+++ config.py
@@ -1,10 +1,4 @@
-from sqlalchemy import create_engine
-
-class Config(object):
-    SECRET_KEY ="CLAVESECRETA"
-    SESSION_COOKIE_SECURE=False
-    
-class DevelopmentConfig(Config):
-    DEBUG=True
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root@localhost:3306/threadline"
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
+try:
+    from config_local import *
+except ImportError:
+    pass
